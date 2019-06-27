a7deb0f83d233dbede6a7e85fe1fcea47f213cf1
Daniel Elero <danixeee@gmail.com>
2019-06-27 17:09:04 +0200

    Add prebuilt OpenSC for Ubuntu16.04

--- oll_sc/__init__.py
+++ oll_sc/__init__.py
@@ -14,7 +14,8 @@ logger = logging.getLogger(__name__)
 OPENSC_LIBS_PATHS = {
     'Darwin-64bit': 'opensc/opensc-pkcs11-Darwin-64bit.so',
     'Windows-64bit': 'opensc/opensc-pkcs11-Windows-64bit.dll',
-    'Windows-32bit': 'opensc/opensc-pkcs11-Windows-32bit.dll'
+    'Windows-32bit': 'opensc/opensc-pkcs11-Windows-32bit.dll',
+    'Linux-64bit': 'opensc/opensc-pkcs11-Linux-64bit.so'  # NOTE: Built on Ubuntu 16.04!
 }
 
 # https://github.com/easybuilders/easybuild/wiki/OS_flavor_name_version
